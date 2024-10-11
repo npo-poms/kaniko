1c889a2397b7165504a2d6eb30d0ee6e7e2133c6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-10-11 18:46:06 +0200

    Made script more complete

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 IMAGE=npo-poms/kaniko
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-docker run -v ~:/root -v $(pwd):/workspace \
+docker run -v ~:/root -v "$(pwd)":/workspace \
     -e PROJECT_VERSION=$PROJECT_VERSION \
     -e IMAGE_TAG=dev \
     -e NAMESPACE=poms \
+    -e CI_COMMIT_REF_NAME="$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)" \
+    -e CI_COMMIT_SHA="$(git show --format="%H"  --no-patch)" \
+    -e CI_COMMIT_TIMESTAMP="$(git show --format="%aI"  --no-patch)" \
+    -e CI_COMMIT_TITLE="$(git show --format="%s"  --no-patch)" \
     $IMAGE
--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 export DOCKER_DIR=`pwd`
 export DOCKER_BUILD_ARGS=  # Uses eval, when overriding escape whitespace: '--build-arg\ "FOO=BAR"'
 export REGISTRY=registry.npohosting.nl # For deploying to openshift, this must be   REGISTRY: openshift-image-registry.apps.cluster.chp4.io. (See also maven_deploy_openshift)
-export DOCKER_AUTH_CONFIG_FILE=$HOME/.kube/config
+export DOCKER_AUTH_CONFIG_FILE=$HOME/.docker/config-gitlab.json
 export KANIKO_ARGS='--cache=false --cache-copy-layers=false'
 export AS_LATEST='false'
 export TRACE='false'
