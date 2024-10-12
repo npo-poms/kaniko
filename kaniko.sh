25b4b73c60348b8d7a83fd91a1c88f82f648cb54
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-10-12 14:34:25 +0200

    cleaning up a bit more.

--- kaniko.sh
+++ kaniko.sh
@@ -2,9 +2,11 @@
 # This script calls kaniko (in docker) for the current directory. You can put it in your path
 # It's actually calling the scripts/script.sh in ghcr.io/npo-poms/kaniko
 
-KANIKO_IMAGE=npo-poms/kaniko
-#KANIKO_IMAGE=ghcr.io/npo-poms/kaniko:2
+
+#KANIKO_IMAGE=npo-poms/kaniko
+KANIKO_IMAGE=ghcr.io/npo-poms/kaniko:3
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Found project version ${PROJECT_VERSION}"
 docker run -v ~/conf:/root/conf -v ~/.docker:/root/.docker -v "$(pwd)":/workspace \
     -e PROJECT_VERSION=$PROJECT_VERSION \
     -e IMAGE_TAG=dev \
