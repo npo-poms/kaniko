daad4c66f20a70f27badebbcf8ac662d79fe3ff2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-10-16 12:53:21 +0200

    Tryuing to make deploy to chp4 work.

--- kaniko.sh
+++ kaniko.sh
@@ -2,16 +2,41 @@
 # This script calls kaniko (in docker) for the current directory. You can put it in your path
 # It's actually calling the scripts/script.sh in ghcr.io/npo-poms/kaniko
 
+REGISTRY=${REGISTRY:-registry.npohosting.nl}
+NAMESPACE=${NAMESPACE:-poms}
 
-#KANIKO_IMAGE=npo-poms/kaniko
-KANIKO_IMAGE=ghcr.io/npo-poms/kaniko:3
+# REGISTRY: openshift-image-registry.apps.cluster.chp4.io.
+
+#echo "Registry: ${REGISTRY}"
+KANIKO_IMAGE=npo-poms/kaniko
+#KANIKO_IMAGE=ghcr.io/npo-poms/kaniko:3
+
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Found project version ${PROJECT_VERSION}"
+
+CI_COMMIT_REF_NAME="$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)"
+
+case $CI_COMMIT_REF_NAME in
+  "main")
+     IMAGE_TAG=dev
+     ;;
+   *SNAPSHOT*)
+    IMAGE_TAG=$(echo "${PROJECT_VERSION}-dev" | tr '[:upper:]' '[:lower:]')
+    ;;
+  *)
+    IMAGE_TAG=$(echo "${PROJECT_VERSION}" | tr '[:upper:]' '[:lower:]')
+    ;;
+esac
+
+echo "image tag $IMAGE_TAG"
+
+
 docker run -v ~/conf:/root/conf -v ~/.docker:/root/.docker -v "$(pwd)":/workspace \
-    -e PROJECT_VERSION=$PROJECT_VERSION \
-    -e IMAGE_TAG=dev \
-    -e NAMESPACE=poms \
-    -e CI_COMMIT_REF_NAME="$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)" \
+    -e PROJECT_VERSION="$PROJECT_VERSION" \
+    -e IMAGE_TAG=${IMAGE_TAG} \
+    -e NAMESPACE=${NAMESPACE} \
+    -e REGISTRY="${REGISTRY}" \
+    -e CI_COMMIT_REF_NAME="${CI_COMMIT_REF_NAME}" \
     -e CI_COMMIT_SHA="$(git show --format="%H"  --no-patch)" \
     -e CI_COMMIT_TIMESTAMP="$(git show --format="%aI"  --no-patch)" \
     -e CI_COMMIT_TITLE="$(git show --format="%s"  --no-patch)" \
--- scripts/script.sh
+++ scripts/script.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+echo "--------"
+echo "Executing $0"
 
 export DOCKER_DIR
 DOCKER_DIR="$(pwd)"
 export DOCKER_BUILD_ARGS=  # Uses eval, when overriding escape whitespace: '--build-arg\ "FOO=BAR"'
-export REGISTRY=registry.npohosting.nl # For deploying to openshift, this must be   REGISTRY: openshift-image-registry.apps.cluster.chp4.io. (See also maven_deploy_openshift)
+export REGISTRY="${REGISTRY:-registry.npohosting.nl}"
 export DOCKER_AUTH_CONFIG_FILE=$HOME/.docker/config-gitlab.json
 export KANIKO_ARGS='--cache=true --cache-copy-layers=true'
 export AS_LATEST='false'
 export TRACE='false'
+
+echo Using registry ${REGISTRY}
 
 . /docker-build-setup.sh
 . /kaniko-gitlab.sh
